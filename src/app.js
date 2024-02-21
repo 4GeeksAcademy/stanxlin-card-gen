d7500f004ed24b3b78ec2d8f96113025e493fd7a
stanxlin <143833064+stanxlin@users.noreply.github.com>
2024-02-21 23:00:02 +0000

    card randomized after refreshing page

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♦", "♥", "♠", "♣"];
+  //get a random # from 1 - 4 for the array index
+  const randomSuitIndex = Math.floor(Math.random() * suits.length);
+  const randomSuit = suits[randomSuitIndex];
+
+  const numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const randomNumberIndex = Math.floor(Math.random() * numbers.length);
+  const randomNumber = numbers[randomNumberIndex];
+
+  const selectedNumberDiv = document.getElementById("number");
+  const selectedIcon1Div = document.getElementById("icon1");
+  const selectedIcon2Div = document.getElementById("icon2");
+
+  selectedNumberDiv.innerHTML = randomNumber;
+  if ((selectedIcon1Div.innerHTML = randomSuit) === "♥") {
+    selectedIcon1Div.style.color = "red";
+  }
+  if ((selectedIcon2Div.innerHTML = randomSuit) === "♥") {
+    selectedIcon2Div.style.color = "red";
+  }
 };
